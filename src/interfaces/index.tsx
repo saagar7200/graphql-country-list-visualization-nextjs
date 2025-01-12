62a7e9e71283b8ec13583f380e015e6f9f36cc6d
Sagar Bhandari <rsaagar7200@gmail.com>
2025-01-12 20:20:46 +0545

    format

--- src/interfaces/index.tsx
+++ src/interfaces/index.tsx
@@ -1,58 +1,54 @@
 // Reusable types for common structures
 export interface Language {
-    code: string;
-    name: string;
-    native: string;
-    rtl: boolean;
-  }
-  
-  export interface State {
-    code: string;
-    name: string;
-  }
-  
-  export interface Country {
-    code: string;
-    name: string;
-    native: string;
-    phone: string;
-    emoji: string;
-    emojiU: string;
-    currency: string | null;
-    languages: Language[];
-    states: State[];
-  }
-  
-  // Queries
-  
-  export interface CountriesQuery {
-    countries: Country[];
-  }
-  
-  export interface CountryQuery {
-    country: Country | null; // `country` can be null if the code doesn't match any country.
-  }
-  
-  export interface ContinentsQuery {
-   
-      code: string;
-      name: string;
-      countries: Country[];
-  
-  }
-  
-  export interface ContinentQuery {
-  
-      code: string;
-      name: string;
-      countries: Country[];
-    }
-  
-  export interface LanguagesQuery {
-    languages: Language[];
-  }
-  
-  export interface LanguageQuery {
-    language: Language | null; // `language` can be null if the code doesn't match any language.
-  }
-  
+  code: string;
+  name: string;
+  native: string;
+  rtl: boolean;
+}
+
+export interface State {
+  code: string;
+  name: string;
+}
+
+export interface Country {
+  code: string;
+  name: string;
+  native: string;
+  phone: string;
+  emoji: string;
+  emojiU: string;
+  currency: string | null;
+  languages: Language[];
+  states: State[];
+}
+
+// Queries
+
+export interface CountriesQuery {
+  countries: Country[];
+}
+
+export interface CountryQuery {
+  country: Country | null; // `country` can be null if the code doesn't match any country.
+}
+
+export interface ContinentsQuery {
+  code: string;
+  name: string;
+  countries: Country[];
+}
+
+export interface ContinentQuery {
+  code: string;
+  name: string;
+  countries: Country[];
+}
+
+export interface LanguagesQuery {
+  languages: Language[];
+}
+
+export interface LanguageQuery {
+  language: Language | null; // `language` can be null if the code doesn't match any language.
+}
